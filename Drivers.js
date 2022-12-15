de165aca9b5846ee7819d0f739f496b7604f17f2
lukas.stojanovski@gmail.com <lstoyanovski@york.citycollege.eu>
2022-12-15 22:07:08 +0200

    nav css fixed

---
+++ Drivers.js
@@ -0,0 +1,9 @@
+// var requestOptions = {
+//     method: 'GET',
+//     redirect: 'follow'
+//   };
+  
+//   fetch("http://ergast.com/api/f1/drivers?=123", requestOptions)
+//     .then(response => response.text())
+//     .then(result => console.log(result))
+//     .catch(error => console.log('error', error));
