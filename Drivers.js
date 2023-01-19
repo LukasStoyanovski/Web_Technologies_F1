fee30b777062a3cf154967cf4645bbf690a5ea71
lukas.stojanovski@gmail.com <lstoyanovski@york.citycollege.eu>
2023-01-19 20:45:03 +0200

    drivers finished

--- Drivers.js
+++ Drivers.js
@@ -1,3 +1,6 @@
+// Created by Lukas Stoyanovski 
+
+
 // function keepDropDown() {
 //     document.getElementById("dropDownMenu").style.display = "block";
 // }
@@ -31,8 +34,6 @@ function driverStats(driver) {
     const object = allDrivers.find(obj => obj.id === driver)
     console.log(object)
     window.location.href = object.page;
-
-
 }
 
 
@@ -59,6 +60,7 @@ function showAllDrivers() {
         <p class="first-name">${driver.first_name}</p>
         <img src="${driver.Country}" class="country"></img>
         <p class="last-name">${driver.last_name}</p>
+        <hr>
         <p class="driver-number">${driver.number}</p>
         <p class="team">${driver.team}</p>
         <img class="driver-photo" src="${driver.photo}"></img>
@@ -86,7 +88,7 @@ var allDrivers = [
         number: 11,
         team: "RedBull Racing",
         teamColor: "rgb(34, 54, 168)",
-        photo: "https://cdn-4.motorsport.com/images/mgl/0a9neZP0/s8/sergio-perez-red-bull-racing-1.jpg",
+        photo: "https://www.formula1.com/content/dam/fom-website/drivers/S/SERPER01_Sergio_Perez/serper01.png.transform/2col-retina/image.png",
         page: "./drivers/SergioPerez.html"
     },
     {
@@ -98,31 +100,31 @@ var allDrivers = [
         number: 1,
         team: "RedBull Racing",
         teamColor: "rgb(34, 54, 168)",
-        photo: "https://cdn-9.motorsport.com/images/mgl/24vA3r46/s8/max-verstappen-red-bull-racing-1.jpg",
+        photo: "https://www.formula1.com/content/dam/fom-website/drivers/M/MAXVER01_Max_Verstappen/maxver01.png.transform/2col-retina/image.png",
         page: "./drivers/MaxVerstappen.html"
     },
     {
-        id: 6,
+        id: 3,
         driver_name: "Charles Leclerc",
         first_name: "Charles",
         last_name: "Leclerc",
         Country: "./flags/EnglandFlag.gif",
         number: 16,
-        teamColor: "rgb(177, 22, 22)",
+        teamColor: "rgb(255, 0, 0)",
         team: "Ferrari",
-        photo:"https://a.espncdn.com/combiner/i?img=/i/headshots/rpm/players/full/5498.png&w=350&h=254",
+        photo:"https://www.formula1.com/content/dam/fom-website/drivers/C/CHALEC01_Charles_Leclerc/chalec01.png.transform/2col-retina/image.png",
         page: "./drivers/CharlesLeclerc.html"
     },
     {
-        id: 13,
+        id: 4,
         driver_name: "Carlos Sainz",
         first_name: "Carlos",
         last_name: "Sainz",
         Country: "./flags/SpainFlag.gif",
         number: 55,
-        teamColor: "rgb(177, 22, 22)",
+        teamColor: "rgb(255, 0, 0)",
         team: "Ferrari",
-        photo: "https://cdn-9.motorsport.com/images/mgl/YXRnk570/s800/carlos-sainz-jr-ferrari-1.jpg",
+        photo: "https://www.formula1.com/content/dam/fom-website/drivers/C/CARSAI01_Carlos_Sainz/carsai01.png.transform/2col-retina/image.png",
         page: "./drivers/CarlosSainz.html"
     },
     {
@@ -134,10 +136,11 @@ var allDrivers = [
         number: 44,
         teamColor: "rgb(119, 235, 196)",
         team: "Mercedes",
-        photo: "https://cdn-1.motorsport.com/images/mgl/0mb95oa2/s800/lewis-hamilton-mercedes-1.jpg"
+        photo: "https://www.formula1.com/content/dam/fom-website/drivers/L/LEWHAM01_Lewis_Hamilton/lewham01.png.transform/2col-retina/image.png",
+        page: "./drivers/LewisHamilton.html"
     },
     {
-        id: 12,
+        id: 6,
         driver_name: "George Russel",
         first_name: "George",
         last_name: "Russel",
@@ -145,40 +148,129 @@ var allDrivers = [
         number: 63,
         teamColor: "rgb(119, 235, 196)",
         team: "Mercedes",
-        photo: "https://cdn-5.motorsport.com/images/mgl/6l9yjoj0/s8/george-russell-mercedes-1.jpg"
+        photo: "https://www.formula1.com/content/dam/fom-website/drivers/G/GEORUS01_George_Russell/georus01.png.transform/2col-retina/image.png",
+        page: "./drivers/GeorgeRussel.html"
     },
     {
-        id: 2,
-        driver_name: "Fernando Alonso",
-        first_name: "Fernando",
-        last_name: "Alonso",
+        id: 7,
+        driver_name: "Esteban Ocon",
+        first_name: "Esteban",
+        last_name: "Ocon",
         Country: "./flags/SpainFlag.gif",
-        number: 14,
+        number: 31,
         teamColor: "rgb(94, 158, 255)",
         team: "Alpine",
-        photo: "https://a.espncdn.com/combiner/i?img=/i/headshots/rpm/players/full/348.png"
+        photo: "https://www.formula1.com/content/dam/fom-website/drivers/E/ESTOCO01_Esteban_Ocon/estoco01.png.transform/2col-retina/image.png",
+        page: "./drivers/EstebanOcon.html"
     },
     {
-        id: 4,
-        driver_name: "Peirre Gasly",
-        first_name: "Peirre",
+        id: 8,
+        driver_name: "Pierre Gasly",
+        first_name: "Pierre",
         last_name: "Gasly",
         Country: "./flags/ItalySmallFlag.jpg",
         number: 10,
         teamColor: "rgb(94, 158, 255)",
         team: "Alpine",
-        photo: "https://a.espncdn.com/combiner/i?img=/i/headshots/rpm/players/full/5501.png"
+        photo: "https://www.formula1.com/content/dam/fom-website/drivers/P/PIEGAS01_Pierre_Gasly/piegas01.png.transform/2col-retina/image.png",
+        page: "./drivers/PierreGasly.html"
     },
-    // {
-    //     id: 1,
-    //     driver_name: "Alexander Albon",
-    //     first_name: "Alexander",
-    //     last_name: "Albon",
-    //     Country: "./flags/JapanSmallFlag.jpg",
-    //     number: 23,
-    //     teamColor: "rgb(190, 216, 255)",
-    //     team: "Williams",
-    //     photo: "https://thesportsgrail.com/wp-content/uploads/2021/09/alex-albon-png-logo-stock-2@2x.jpg"
-    // },
+    {
+        id: 9,
+        driver_name: "Lando Norris",
+        first_name: "Lando",
+        last_name: "Norris",
+        Country: "./flags/EnglandFlag.gif",
+        number: 4,
+        teamColor: "rgb(240, 127, 52)",
+        team: "Mclaren",
+        photo: "https://www.formula1.com/content/dam/fom-website/drivers/L/LANNOR01_Lando_Norris/lannor01.png.transform/2col-retina/image.png",
+        page: "./drivers/LandoNorris.html"
+    },
+    {
+        id: 10,
+        driver_name: "Valteri Bottas",
+        first_name: "Valteri",
+        last_name: "Bottas",
+        Country: "./flags/BelgiumSmallFlag.jpg",
+        number: 77,
+        teamColor: "rgb(150, 2, 2)",
+        team: "Alpha Romeo",
+        photo: "https://www.formula1.com/content/dam/fom-website/drivers/V/VALBOT01_Valtteri_Bottas/valbot01.png.transform/2col-retina/image.png",
+        page: "./drivers/ValteriBottas.html"
+    },
+    {
+        id: 11,
+        driver_name: "Zhou Guanyu",
+        first_name: "Zhou",
+        last_name: "Guanyu",
+        Country: "./flags/JapanSmallFlag.jpg",
+        number: 24,
+        teamColor: "rgb(150, 2, 2)",
+        team: "Alpha Romeo",
+        photo: "https://www.formula1.com/content/dam/fom-website/drivers/G/GUAZHO01_Guanyu_Zhou/guazho01.png.transform/2col-retina/image.png",
+        page: "./drivers/ZhouGuanyu.html"
+    },
+    {
+        id: 12,
+        driver_name: "Alexander Albon",
+        first_name: "Alexander",
+        last_name: "Albon",
+        Country: "./flags/PolandSmallFlag.jpg",
+        number: 23,
+        teamColor: "rgb(0, 225, 255)",
+        team: "Williams",
+        photo: "https://www.formula1.com/content/dam/fom-website/drivers/A/ALEALB01_Alexander_Albon/alealb01.png.transform/2col-retina/image.png",
+        page: "./drivers/AlexanderAlbon.html"
+    },
+    {
+        id: 13,
+        driver_name: "Lance Stroll",
+        first_name: "Lance",
+        last_name: "Stroll",
+        Country: "./flags/CanadaSmallFlag.jpg",
+        number: 18,
+        teamColor: "rgb(0, 97, 84)",
+        team: "Aston Martin",
+        photo: "https://www.formula1.com/content/dam/fom-website/drivers/L/LANSTR01_Lance_Stroll/lanstr01.png.transform/2col-retina/image.png",
+        page: "./drivers/LanceStroll.html"
+    },
+    {
+        id: 14,
+        driver_name: "Fernando Alonso",
+        first_name: "Fernando",
+        last_name: "Alonso",
+        Country: "./flags/SpainFlag.gif",
+        number: 14,
+        teamColor: "rgb(0, 97, 84)",
+        team: "Aston Martin",
+        photo: "https://www.formula1.com/content/dam/fom-website/drivers/F/FERALO01_Fernando_Alonso/feralo01.png.transform/2col-retina/image.png",
+        page: "./drivers/FernandoAlonso.html"
+    },
+    {
+        id: 15,
+        driver_name: "Kevin Magnussen",
+        first_name: "Kevin",
+        last_name: "Magnussen",
+        Country: "./flags/BelgiumSmallFlag.jpg",
+        number: 20,
+        teamColor: "gray",
+        team: "Faas F1 Team",
+        photo: "https://www.formula1.com/content/dam/fom-website/drivers/K/KEVMAG01_Kevin_Magnussen/kevmag01.png.transform/2col-retina/image.png",
+        page: "./drivers/KevinMagnussen.html"
+    },
+    {
+        id: 16,
+        driver_name: "Yuki Tsunoda",
+        first_name: "Yuki",
+        last_name: "Tsunoda",
+        Country: "./flags/JapanSmallFlag.jpg",
+        number: 22,
+        teamColor: "rgb(0, 72, 105)",
+        team: "AlphaTauri",
+        photo: "https://www.formula1.com/content/dam/fom-website/drivers/Y/YUKTSU01_Yuki_Tsunoda/yuktsu01.png.transform/2col-retina/image.png",
+        page: "./drivers/YukiTsunoda.html"
+    },
+    
     
 ]
